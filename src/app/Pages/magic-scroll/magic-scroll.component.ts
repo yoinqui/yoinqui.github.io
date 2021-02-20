70231d8ff4bd67fcc2dc8de9c6d08e7254d5f2e5
yoinqui <yoann.inquimbert@viacesi.fr>
2021-02-20 20:21:53 +0100

    Resolve Conflits & MagicScroll

--- src/app/Pages/magic-scroll/magic-scroll.component.ts
+++ src/app/Pages/magic-scroll/magic-scroll.component.ts
@@ -11,6 +11,20 @@ export class MagicScrollComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.UpdateSkewedValuesElements();
   }
 
+  private UpdateSkewedValuesElements(): void {
+    const SkewedOne: Element = document.querySelector('.SkewedOne');
+    const SkewedTwo: Element = document.querySelector('.SkewedTwo');
+
+    // tslint:disable-next-line:only-arrow-functions
+    window.addEventListener('scroll', function(): void {
+      const value1 = -12 + window.scrollY / 45;
+      const value2 = 12 + window.scrollY / -45;
+
+      SkewedOne.setAttribute('style', 'transform: skewY(' + value1 + 'deg)');
+      SkewedTwo.setAttribute('style', 'transform: skewY(' + value2 + 'deg)');
+    });
+  }
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import { MagicScrollComponent } from './Pages/magic-scroll/magic-scroll.component';
 
+
 const routes: Routes = [
   { path: 'MagicScroll', component: MagicScrollComponent }
 ];
