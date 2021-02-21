3243db00e7fc456832f70fe5745b363c89e179d6
yoinqui <yoann.inquimbert@viacesi.fr>
2021-02-21 18:12:23 +0100

    Add Analig-Clock Page & Add routing

---
+++ src/app/Pages/analog-clock/analog-clock.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-analog-clock',
+  templateUrl: './analog-clock.component.html',
+  styleUrls: ['./analog-clock.component.css']
+})
+export class AnalogClockComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
