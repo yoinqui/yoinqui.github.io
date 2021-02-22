1a378adaec42d441a897ef16161d0d207bcb7a72
yoinqui <yoann.inquimbert@viacesi.fr>
2021-02-22 22:28:30 +0100

    Neumorphism function -- not finish -- White Sphere Width & Reload Page after AnalogClock Page

--- src/app/Pages/neumorphism/neumorphism.component.ts
+++ src/app/Pages/neumorphism/neumorphism.component.ts
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NeumorphismComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
