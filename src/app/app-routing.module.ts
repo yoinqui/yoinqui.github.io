56b603f250f714ec024623c978a9b781afb356fe
Yoann INQUIMBERT <yoann.inquimbert@viacesi.fr>
2021-03-10 23:07:18 +0100

    Init commit -- Add Focus Page & Add Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import {MagicScrollComponent} from './Pages/magic-scroll/magic-scroll.component'
 import {HomeComponent} from './Pages/home/home.component';
 import {AnalogClockComponent} from './Pages/analog-clock/analog-clock.component';
 import {NeumorphismComponent} from './Pages/neumorphism/neumorphism.component';
+import {FocusComponent} from './Pages/focus/focus.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'MagicScroll', component: MagicScrollComponent},
   {path: 'AnalogClock', component: AnalogClockComponent},
   {path: 'Neumorphism', component: NeumorphismComponent},
+  {path: 'Focus', component: FocusComponent},
 ];
 
 @NgModule({
