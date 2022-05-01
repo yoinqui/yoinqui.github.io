d25c5960a5fbf9be146399971069142fc56b6de6
yoinqui <yoann.inquimbert@viacesi.fr>
2022-05-01 15:50:03 +0200

    Popup Video Function

--- src/app/Pages/popup-video/popup-video.component.ts
+++ src/app/Pages/popup-video/popup-video.component.ts
@@ -10,6 +10,25 @@ export class PopupVideoComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.LoadVideoPopup();
   }
 
+  private LoadVideoPopup(): void {
+    let btn = document.getElementById('btn');
+    let clip = document.getElementById('clip');
+    let video = document.querySelector('video');
+    let close = document.getElementById('close');
+    console.log(btn);
+    btn.onclick = function(){
+      btn.classList.add("active");
+      clip.classList.add("active");
+      video.play();
+      video.currentTime = 0;
+    }
+    close.onclick = function(){
+      btn.classList.remove("active");
+      clip.classList.remove("active");
+      video.pause();
+    }
+  }
 }
