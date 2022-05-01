d3522c1fbd400d4acb7b323cc4d33b6dd623e91a
yoinqui <yoann.inquimbert@viacesi.fr>
2022-05-01 17:31:41 +0200

    IssToolbarVisible function - To delete Toolbar display in Coming Sonn Page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PersoProject';
+  isVisible: boolean;
+
+  public isToolbarVisible(): void {
+     this.isVisible = document.location.pathname != "/ComingSoon";
+  }
 }
