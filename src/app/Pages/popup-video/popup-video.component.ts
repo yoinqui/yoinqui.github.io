208ae5ec177243a32a79ae65c3086ade738dad33
yoinqui <yoann.inquimbert@viacesi.fr>
2022-12-15 19:40:48 +0100

    Update url youtube iframe

--- src/app/Pages/popup-video/popup-video.component.ts
+++ src/app/Pages/popup-video/popup-video.component.ts
@@ -16,19 +16,19 @@ export class PopupVideoComponent implements OnInit {
   private LoadVideoPopup(): void {
     let btn = document.getElementById('btn');
     let clip = document.getElementById('clip');
-    let video = document.querySelector('video');
+    var iframe = document.querySelector('iframe');
     let close = document.getElementById('close');
-    console.log(btn);
-    btn.onclick = function(){
+
+    btn.onclick = function () {
       btn.classList.add("active");
       clip.classList.add("active");
-      video.play();
-      video.currentTime = 0;
+      iframe.classList.remove("desactive");
     }
-    close.onclick = function(){
+
+    close.onclick = function () {
       btn.classList.remove("active");
       clip.classList.remove("active");
-      video.pause();
+      iframe.classList.add("desactive");
     }
   }
 }
